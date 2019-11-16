23fc8d8227d7d8acc514413231088dafe2e32b8f
Suzana de Melo de Moraes <suzanamelojor@gmail.com>
2019-11-16 15:57:01 +1300

    create server envirolment

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 const path = require('path')
 const express = require('express')
 
+const router = require('./routes/routes')
+
 const server = express()
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
+
+server.use('/v1', router)
+
 
 module.exports = server
