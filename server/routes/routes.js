1bfad48593e4d999e48406cec39626cd2bf6c3c0
Suzana de Melo de Moraes <suzanamelojor@gmail.com>
2019-11-16 15:58:12 +1300

    create router envirolment and get categories

---
+++ server/routes/routes.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+const router = express.Router()
+
+const db = require('../db/db')
+
+router.get('/categories', (req, res) => {
+  db.getCategories()
+    .then(displayCategories)
+    .catch(err => res.status(500).send(err.message))
+
+  function displayCategories (categories) {
+    res.json(categories)
+  }
+})
+
+router.getItems()
+
+module.exports = router
