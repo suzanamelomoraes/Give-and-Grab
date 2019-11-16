735a00b3246d583fb8f89a93c273745696b88f75
Suzana de Melo de Moraes <suzanamelojor@gmail.com>
2019-11-16 15:59:03 +1300

    create db.js and getcategories from database

---
+++ server/db/db.js
@@ -0,0 +1,18 @@
+const environment = process.env.NODE_ENV || 'development'
+const config = require('../../knexfile')[environment]
+const connection = require('knex')(config)
+
+module.exports = {
+  getCategories
+}
+
+function getCategories (db = connection) {
+  return db('categories')
+    .select()
+}
+
+function getItemsById (id, db = connection) {
+  return db('items')
+    .where(cat_id, id)
+    .select('title', 'description', 'email', 'image_path')
+}
