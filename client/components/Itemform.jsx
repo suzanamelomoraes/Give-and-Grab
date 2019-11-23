df57ef08555c5e86c656e4e7705ffb1fe9626406
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2019-11-24 08:22:33 +1300

    create state and onchange method in Itemsform

--- client/components/Itemform.jsx
+++ client/components/Itemform.jsx
@@ -1,6 +1,16 @@
 import React, { Component } from "react";
 
 class Itemform extends Component {
+  state = {
+    title: "",
+    email: "",
+    description: ""
+  };
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
   render() {
     return (
       <div>
@@ -8,19 +18,33 @@ class Itemform extends Component {
           <div>
             <label>Name:</label>
             <br />
-            <input type="text" name="title" />
+            <input
+              type="text"
+              name="title"
+              onChange={this.onChange}
+              value={this.state.title}
+            />
           </div>
           <br />
           <div>
             <label>Email:</label>
             <br />
-            <input type="text" name="email" />
+            <input
+              type="text"
+              name="email"
+              onChange={this.onChange}
+              value={this.state.email}
+            />
           </div>
           <br />
           <div>
             <label>Description:</label>
             <br />
-            <textarea name="description" />
+            <textarea
+              name="description"
+              onChange={this.onChange}
+              value={this.state.description}
+            />
           </div>
           <br />
           <button type="submit">Submit</button>
--- client/components/Items.jsx
+++ client/components/Items.jsx
@@ -1,31 +1,43 @@
-import React from 'react'
-import { getItems } from '../api'
+import React from "react";
+import { getItems } from "../api";
+import Itemform from "./Itemform";
 
 class Items extends React.Component {
   state = {
     itemsState: []
   };
 
-  componentDidMount () {
+  componentDidMount() {
     getItems(this.props.categoryId).then(itemsData => {
-      this.setState({ itemsState: itemsData })
-    })
+      this.setState({ itemsState: itemsData });
+    });
   }
 
   // getItems(cat.id = key)
   // needs to receive categories.id to send to the rest api
 
-  render () {
+  render() {
     return (
       <ul>
         {this.state.itemsState.map(item => {
-
-          return <li key={item.id}> {item.title} <br/>{item.description} <br/>
-            {item.email} <br/><img height="250" width="250" src={`/images/${item.image_path}.jpg`}/> </li>
+          return (
+            <li key={item.id}>
+              {" "}
+              {item.title} <br />
+              {item.description} <br />
+              {item.email} <br />
+              <img
+                height="250"
+                width="250"
+                src={`/images/${item.image_path}.jpg`}
+              />{" "}
+            </li>
+          );
         })}
+        <Itemform />
       </ul>
-    )
+    );
   }
 }
 
-export default Items
+export default Items;
