82b7e44544969809a8fbbbe470847a84009c9832
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-01-08 10:27:09 +1300

    move route related to categories table from routes.js to categories.js

---
+++ server/routes/categories.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const router = express.Router();
+
+const db = require("../db/db");
+
+router.get("/categories", (req, res) => {
+  db.getCategories()
+    .then(displayCategories)
+    .catch(err => res.status(500).send(err.message));
+
+  function displayCategories(categories) {
+    res.json(categories);
+  }
+});
