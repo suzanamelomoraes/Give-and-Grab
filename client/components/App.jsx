cdf30bbe09045d8cf09a031f60c963e590cfcd96
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-06-15 22:56:57 +1200

    import and apply grid to define the layout

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
-import Header from './Header'
-import Categories from './Categories'
-import Button from './Button'
+import React from 'react';
+import Header from './Header';
+import Categories from './Categories';
+import Button from './Button';
+import { Grid } from '@material-ui/core';
 
 const App = () => {
   return (
-    <>
-    <Header/>
-    <Categories/>
-    <Button/>
-    </>
-  )
-}
+    <Grid container direction='column'>
+      <Grid item>
+        <Header />
+      </Grid>
+      <Grid item container>
+        <Grid item xs={false} sm={2} />
+        <Grid item xs={12} sm={8}>
+          <Categories />
+        </Grid>
+        <Grid item xs={false} sm={2} />
+      </Grid>
+      <Grid item>
+        <Button />
+      </Grid>
+    </Grid>
+  );
+};
 
-export default App
+export default App;
