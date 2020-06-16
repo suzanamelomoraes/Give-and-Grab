f2da4874edac7ed865bf9acc8c2b8b86210ed6b3
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-06-16 18:16:52 +1200

    import and apply material ui components to style the header such as AppBar, typography and icons

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles(() => ({
+  typographyStyles: {
+    flex: 1,
+  },
+}));
 
 const Header = () => {
+  const classes = useStyles();
   return (
     <AppBar position='static'>
       <Toolbar>
-        <Typography>Give & Grab</Typography>
+        <Typography variant='h4' className={classes.typographyStyles}>
+          Give & Grab
+        </Typography>
+        <FavoriteIcon />
       </Toolbar>
     </AppBar>
   );
